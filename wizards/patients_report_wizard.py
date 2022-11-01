30fb92a393b46121649790700b5b9ef9b7905f50
chinmayroy <mrchinmayroy@gmail.com>
2022-11-01 17:56:52 +0600

    update

--- wizards/patients_report_wizard.py
+++ wizards/patients_report_wizard.py
@@ -5,14 +5,14 @@ class PatientsReportWizard(models.TransientModel):
     _name = "patients.report.wizard"
     _description = "Patients Report Wizard"
     
-    patient_sl_no = fields.Many2one("hospital.support.patients.informations", string="Patient SL No:")
+    patient_name = fields.Many2one("hospital.support.patients.informations", string="Patient SL No:")
     date_from = fields.Date(string="Date From:")
     date_to = fields.Date(string="Date To:")
     
 
     def action_print_patients_report(self):
-        patients_report_w = self.env['hospital.support.patients.informations'].sudo().search([('create_date', '=', self.date_from), ('create_date', '=', self.date_to)])
-        # patients_report_w = self.env['hospital.support.patients.informations'].search_read()
+        # patients_report_w = self.env['hospital.support.patients.informations'].sudo().search([('create_date', '=', self.date_from), ('create_date', '=', self.date_to)])
+        patients_report_w = self.env['hospital.support.patients.informations'].search_read()
         print("patients_report_w", patients_report_w)
         data = {
             'form_data': self.read()[0],
