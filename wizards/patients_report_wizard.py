77ce92c49fe05ee64bb37706e7c7667d124e6a26
chinmayroy <mrchinmayroy@gmail.com>
2022-11-01 19:05:06 +0600

    update

--- wizards/patients_report_wizard.py
+++ wizards/patients_report_wizard.py
@@ -11,11 +11,24 @@ class PatientsReportWizard(models.TransientModel):
     
 
     def action_print_patients_report(self):
-        # patients_report_w = self.env['hospital.support.patients.informations'].sudo().search([('create_date', '=', self.date_from), ('create_date', '=', self.date_to)])
-        patients_report_w = self.env['hospital.support.patients.informations'].search_read()
-        print("patients_report_w", patients_report_w)
+        datalist = []
+        patients_report_w = self.env['hospital.support.patients.informations'].sudo().search([('create_date', '>=', self.date_from), ('create_date', '<=', self.date_to)])
+        # patients_report_w = self.env['hospital.support.patients.informations'].search_read()
+        print("==================================================================================")
+        print(patients_report_w)
+        print("==================================================================================")
+        for line in patients_report_w:
+            values = {
+                'sl_no': line.sl_no,
+                'name': line.name,
+                'mobile': line.mobile,
+                'address': line.address,
+                'diseases': line.diseases,
+                'description': line.description,
+            }
+            datalist.append(values)
         data = {
             'form_data': self.read()[0],
-            'patients_report_w': patients_report_w
+            'patients_report_w': datalist
         }
         return self.env.ref('hospital_support.action_patients_report_wizard').report_action(self, data=data)
